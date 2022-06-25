582612c5b9cac7171bfa8e793ac4f8476aba0aa3
rangolla <ranganadh1509@gmail.com>
2022-06-25 16:52:09 +0530

    commit

--- public/app.js
+++ public/app.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 
+var todoController = require('./controllers/todoController');
+
 //set up template engine
 app.set('view engine', 'ejs');
 
 //static files
 app.use(express.static('./public'));
 
+//fire controllers
+todoController(app);
+
 //listen to port
-app.listen(3000);
+app.listen(5000);
 console.log('You are listening to port 3000');
